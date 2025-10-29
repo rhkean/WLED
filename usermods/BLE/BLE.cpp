4976d500cb36e1aed40192e4782058c5e1096e8c
Rob Kean <rhkean@gmail.com>
2025-10-29 11:28:16 -0400

    Update BLE.cpp and BLE.h

--- usermods/BLE/BLE.cpp
+++ usermods/BLE/BLE.cpp
@@ -1,6 +1,7 @@
 #ifdef USERMOD_BLE
 #include "BLE.h"
 #include <esp_bt_device.h>
+#include <HTTPClient.h>
 
 const char BLEUsermod::_name[]       PROGMEM = "BLE";
 const char BLEUsermod::_enabled[]    PROGMEM = "enabled";
@@ -65,10 +66,6 @@ void BLEUsermod::loop()
     }
 
     if (millis() - lastTime > 2000 / portTICK_PERIOD_MS) {
-        // updateStateCharacteristic();
-        // updateInfoCharacteristic();
-        // updateEffectsCharacteristic();
-        // updatePalettesCharacteristic();
         handleJson();
         lastTime = millis();
     }
@@ -119,14 +116,14 @@ int BLEUsermod::read() {
 size_t BLEUsermod::write(const uint8_t* buffer, size_t bufferSize){
     if (!isConnected()) return 0;
     DEBUG_PRINTLN(F("long write called"));
-    pTxCharacteristic->setValue(const_cast<uint8_t*>(buffer), bufferSize);
+    pJsonTxCharacteristic->setValue(const_cast<uint8_t*>(buffer), bufferSize);
     return bufferSize;
 }
 
 size_t BLEUsermod::write(uint8_t byte) {
     if (!isConnected()) return 0;
     DEBUG_PRINTLN(F("short write called"));
-    pTxCharacteristic->setValue(&byte, 1);
+    pJsonTxCharacteristic->setValue(&byte, 1);
     return 1;
 }
 
@@ -143,8 +140,8 @@ void BLEUsermod::begin() {
                                                  WLED_BLE_UUID_4TH_VALUE));
     if(!pService) return;
 
-    pRxCharacteristic =
-        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_RX_OFFSET,
+    pJsonRxCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_JSON_RX_OFFSET,
                                                   WLED_BLE_UUID_2ND_VALUE,
                                                   WLED_BLE_UUID_3RD_VALUE,
                                                   WLED_BLE_UUID_4TH_VALUE),
@@ -152,16 +149,250 @@ void BLEUsermod::begin() {
                                        | NIMBLE_PROPERTY::WRITE_NR,
                                        4096);
 
-    pTxCharacteristic =
-        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_TX_OFFSET,
+    pJsonTxCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_JSON_TX_OFFSET,
                                                   WLED_BLE_UUID_2ND_VALUE,
                                                   WLED_BLE_UUID_3RD_VALUE,
                                                   WLED_BLE_UUID_4TH_VALUE),
                                        NIMBLE_PROPERTY::READ
                                        | NIMBLE_PROPERTY::NOTIFY,
                                        4096);
+    pJsonRxCharacteristic->setCallbacks(new BLEUsermodCharacteristicCallbacks(&receiveBuffer));
 
-    pRxCharacteristic->setCallbacks(new BLEUsermodCharacteristicCallbacks(&receiveBuffer));
+#ifndef WLED_DISABLE_2D 
+    pHttpLiveview2DCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_LIVEVIEW2D_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_liveviewws2D_length);
+    pHttpLiveview2DCharacteristic->setValue((uint8_t*)PAGE_liveviewws2D, PAGE_liveviewws2D_length);
+    pHttpLiveview2DCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED 2D Live View");
+#endif
+
+    pHttpLiveviewCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_LIVEVIEW_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_liveview_length);
+    pHttpLiveviewCharacteristic->setValue((uint8_t*)PAGE_liveview, PAGE_liveview_length);
+    pHttpLiveviewCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Live View");
+
+    pHttpCommonJsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_COMMONJS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       JS_common_length);
+    pHttpCommonJsCharacteristic->setValue((uint8_t*)JS_common, JS_common_length);
+    pHttpCommonJsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Common JS");
+
+    pHttpStyleCssCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_STYLECSS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settingsCss_length);
+    pHttpStyleCssCharacteristic->setValue((uint8_t*)PAGE_settingsCss, PAGE_settingsCss_length);
+    pHttpStyleCssCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Settings CSS");
+
+    pHttpFaviconCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_FAVICON_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       favicon_length);
+    pHttpFaviconCharacteristic->setValue((uint8_t*)favicon, favicon_length);
+    pHttpFaviconCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Favicon");
+
+    pHttpIndexCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_INDEX_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_index_length);
+    pHttpIndexCharacteristic->setValue((uint8_t*)PAGE_index, PAGE_index_length);
+    pHttpIndexCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Main");
+
+#ifdef WLED_ENABLE_PIXART
+    pHttpPixartCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_PIXART_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_pixart_length);
+    pHttpPixartCharacteristic->setValue((uint8_t*)PAGE_pixart, PAGE_pixart_length);
+    pHttpPixartCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED PixArt");
+#endif
+#ifndef WLED_DISABLE_PXMAGIC
+    pHttpPxmagicCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_PXMAGIC_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_pxmagic_length);
+    pHttpPxmagicCharacteristic->setValue((uint8_t*)PAGE_pxmagic, PAGE_pxmagic_length);
+    pHttpPxmagicCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED PxMagic");
+#endif
+    pHttpCpalCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_CPAL_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_cpal_length);
+    pHttpCpalCharacteristic->setValue((uint8_t*)PAGE_cpal, PAGE_cpal_length);
+    pHttpCpalCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Color Palettes");
+
+    pHttpWifiSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_WIFI_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_wifi_length);
+    pHttpWifiSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_wifi, PAGE_settings_wifi_length);
+    pHttpWifiSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED WiFi Settings");
+
+    pHttpLedsSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_LEDS_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_leds_length);
+    pHttpLedsSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_leds, PAGE_settings_leds_length);
+    pHttpLedsSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED LED Settings");
+
+    pHttpUiSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_UI_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_ui_length);
+    pHttpUiSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_ui, PAGE_settings_ui_length);
+    pHttpUiSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED UI Settings");
+
+    pHttpSyncSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_SYNC_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_sync_length);
+    pHttpSyncSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_sync, PAGE_settings_sync_length);
+    pHttpSyncSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Sync Settings");
+
+    pHttpTimeSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_TIME_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_time_length);
+    pHttpTimeSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_time, PAGE_settings_time_length);
+    pHttpTimeSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Time Settings");
+
+    pHttpSecuritySettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_SECURITY_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_sec_length);
+    pHttpSecuritySettingsCharacteristic->setValue((uint8_t*)PAGE_settings_sec, PAGE_settings_sec_length);
+    pHttpSecuritySettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Security Settings");
+
+    pHttpDmxSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_DMX_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_dmx_length);
+    pHttpDmxSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_dmx, PAGE_settings_dmx_length);
+    pHttpDmxSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED DMX Settings");
+
+    pHttpUmodsSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_UMODS_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_um_length);
+    pHttpUmodsSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_um, PAGE_settings_um_length);
+    pHttpUmodsSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Usermod Settings");
+
+    pHttpUpdateCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_UPDATE_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_update_length);
+    pHttpUpdateCharacteristic->setValue((uint8_t*)PAGE_update, PAGE_update_length);
+    pHttpUpdateCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Update Page");
+
+    pHttp2DSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_2D_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_2D_length);
+    pHttp2DSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_2D, PAGE_settings_2D_length);
+    pHttp2DSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED 2D Settings");
+
+    pHttpPinSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_PIN_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_pin_length);
+    pHttpPinSettingsCharacteristic->setValue((uint8_t*)PAGE_settings_pin, PAGE_settings_pin_length);
+    pHttpPinSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Pin Settings");
+
+    pHttpCssSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_CSS_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settingsCss_length);
+    pHttpCssSettingsCharacteristic->setValue((uint8_t*)PAGE_settingsCss, PAGE_settingsCss_length);
+    pHttpCssSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Settings CSS");
+
+    pHttpWelcomeCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_WELCOME_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_welcome_length);
+    pHttpWelcomeCharacteristic->setValue((uint8_t*)PAGE_welcome, PAGE_welcome_length);
+    pHttpWelcomeCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Welcome Page");
+
+    pHttpSettingsCharacteristic =
+        pService->createCharacteristic(NimBLEUUID(WLED_BLE_UUID_1ST_VALUE + WLED_BLE_HTTP_SETTINGS_OFFSET,
+                                                  WLED_BLE_UUID_2ND_VALUE,
+                                                  WLED_BLE_UUID_3RD_VALUE,
+                                                  WLED_BLE_UUID_4TH_VALUE),
+                                       NIMBLE_PROPERTY::READ,
+                                       PAGE_settings_length);
+    pHttpSettingsCharacteristic->setValue((uint8_t*)PAGE_settings, PAGE_settings_length);
+    pHttpSettingsCharacteristic->createDescriptor("2901", NIMBLE_PROPERTY::READ, 23)->setValue("WLED Settings Main");
+    
 
     if(!pService->start()) return;
 
--- usermods/BLE/BLE.h
+++ usermods/BLE/BLE.h
@@ -5,14 +5,43 @@
 #include <NimBLEDevice.h>
 #include <queue>
 #include "wled.h"
+#include "html.h"
 
-const uint32_t WLED_BLE_UUID_1ST_VALUE = 0x6E400000UL;
-const uint16_t WLED_BLE_UUID_2ND_VALUE = 0xB5A3U; 
-const uint16_t WLED_BLE_UUID_3RD_VALUE = 0xF393U;
-const uint64_t WLED_BLE_UUID_4TH_VALUE = 0xE0A9E50E24DCCA9ELLU;
-#define WLED_BLE_SERVICE_OFFSET 1
-#define WLED_BLE_RX_OFFSET      2
-#define WLED_BLE_TX_OFFSET      3
+//"01FA0001-46C9-4507-84BB-F2BE3F24C47A"
+const uint32_t WLED_BLE_UUID_1ST_VALUE = 0x01FA0001UL;
+const uint16_t WLED_BLE_UUID_2ND_VALUE = 0x46C9U; 
+const uint16_t WLED_BLE_UUID_3RD_VALUE = 0x4507U;
+const uint64_t WLED_BLE_UUID_4TH_VALUE = 0x84BBF2BE3F24C47ALLU;
+#define WLED_BLE_SERVICE_OFFSET                  1
+#define WLED_BLE_JSON_RX_OFFSET                  2
+#define WLED_BLE_JSON_TX_OFFSET                  3
+#define WLED_BLE_HTTP_LIVEVIEW2D_OFFSET          4
+#define WLED_BLE_HTTP_LIVEVIEW_OFFSET            5
+#define WLED_BLE_HTTP_COMMONJS_OFFSET            6
+#define WLED_BLE_HTTP_STYLECSS_OFFSET            7
+#define WLED_BLE_HTTP_FAVICON_OFFSET             8
+#define WLED_BLE_HTTP_INDEX_OFFSET               9
+#ifdef WLED_ENABLE_PIXART
+#define WLED_BLE_HTTP_PIXART_OFFSET             10
+#endif
+#ifndef WLED_DISABLE_PXMAGIC
+#define WLED_BLE_HTTP_PXMAGIC_OFFSET            11
+#endif
+#define WLED_BLE_HTTP_CPAL_OFFSET               12
+#define WLED_BLE_HTTP_WIFI_SETTINGS_OFFSET      13
+#define WLED_BLE_HTTP_LEDS_SETTINGS_OFFSET      14
+#define WLED_BLE_HTTP_UI_SETTINGS_OFFSET        15
+#define WLED_BLE_HTTP_SYNC_SETTINGS_OFFSET      16
+#define WLED_BLE_HTTP_TIME_SETTINGS_OFFSET      17
+#define WLED_BLE_HTTP_SECURITY_SETTINGS_OFFSET  18
+#define WLED_BLE_HTTP_DMX_SETTINGS_OFFSET       19
+#define WLED_BLE_HTTP_UMODS_SETTINGS_OFFSET     20
+#define WLED_BLE_HTTP_UPDATE_OFFSET             21
+#define WLED_BLE_HTTP_2D_SETTINGS_OFFSET        22
+#define WLED_BLE_HTTP_PIN_SETTINGS_OFFSET       23
+#define WLED_BLE_HTTP_CSS_SETTINGS_OFFSET       24
+#define WLED_BLE_HTTP_WELCOME_OFFSET            25
+#define WLED_BLE_HTTP_SETTINGS_OFFSET           26
 
 class BLEUsermodServerCallbacks: public NimBLEServerCallbacks
 {
@@ -48,8 +77,37 @@ class BLEUsermod : public Usermod,
 
     NimBLEServer* pServer = nullptr;
     NimBLEService* pService = nullptr;
-    NimBLECharacteristic* pRxCharacteristic = nullptr;
-    NimBLECharacteristic* pTxCharacteristic = nullptr;
+    NimBLECharacteristic* pJsonRxCharacteristic                 = nullptr;
+    NimBLECharacteristic* pJsonTxCharacteristic                 = nullptr;
+#ifndef WLED_DISABLE_2D
+    NimBLECharacteristic* pHttpLiveview2DCharacteristic         = nullptr;
+#endif
+    NimBLECharacteristic* pHttpLiveviewCharacteristic           = nullptr;
+    NimBLECharacteristic* pHttpCommonJsCharacteristic           = nullptr;
+    NimBLECharacteristic* pHttpStyleCssCharacteristic           = nullptr;
+    NimBLECharacteristic* pHttpFaviconCharacteristic            = nullptr;
+    NimBLECharacteristic* pHttpIndexCharacteristic              = nullptr;
+#ifdef WLED_ENABLE_PIXART
+    NimBLECharacteristic* pHttpPixartCharacteristic             = nullptr;
+#endif
+#ifndef WLED_DISABLE_PXMAGIC
+    NimBLECharacteristic* pHttpPxmagicCharacteristic            = nullptr;
+#endif
+    NimBLECharacteristic* pHttpCpalCharacteristic               = nullptr;
+    NimBLECharacteristic* pHttpWifiSettingsCharacteristic       = nullptr;
+    NimBLECharacteristic* pHttpLedsSettingsCharacteristic       = nullptr;
+    NimBLECharacteristic* pHttpUiSettingsCharacteristic         = nullptr;
+    NimBLECharacteristic* pHttpSyncSettingsCharacteristic       = nullptr;
+    NimBLECharacteristic* pHttpTimeSettingsCharacteristic       = nullptr;
+    NimBLECharacteristic* pHttpSecuritySettingsCharacteristic   = nullptr;
+    NimBLECharacteristic* pHttpDmxSettingsCharacteristic        = nullptr;
+    NimBLECharacteristic* pHttpUmodsSettingsCharacteristic      = nullptr;
+    NimBLECharacteristic* pHttpUpdateCharacteristic             = nullptr;
+    NimBLECharacteristic* pHttp2DSettingsCharacteristic         = nullptr;
+    NimBLECharacteristic* pHttpPinSettingsCharacteristic        = nullptr;
+    NimBLECharacteristic* pHttpCssSettingsCharacteristic        = nullptr;
+    NimBLECharacteristic* pHttpWelcomeCharacteristic            = nullptr;
+    NimBLECharacteristic* pHttpSettingsCharacteristic           = nullptr;
   
     public:
     BLEUsermod();
@@ -71,7 +129,7 @@ class BLEUsermod : public Usermod,
     int read() override;
     size_t write(const uint8_t* buffer, size_t bufferSize) override;
     size_t write(uint8_t byte) override;
-    void flush(void) override { this->pTxCharacteristic->notify(true); }
+    void flush(void) override { this->pJsonTxCharacteristic->notify(true); }
     // size_t println(void) { Stream::write("\n"); flush(); return 1; }
 
     void handleJson();
@@ -79,8 +137,8 @@ class BLEUsermod : public Usermod,
     bool isEnabled() { return enabled; }
     bool isAdvertising() { return NimBLEDevice::getAdvertising()->isAdvertising(); }
     bool isConnected() { return pServer != nullptr && pServer->getConnectedCount() > 0; }
-    auto getRxCharacteristic() -> NimBLECharacteristic* { return pRxCharacteristic; }
-    auto getTxCharacteristic() -> NimBLECharacteristic* { return pTxCharacteristic; }
+    auto getRxCharacteristic() -> NimBLECharacteristic* { return pJsonRxCharacteristic; }
+    auto getTxCharacteristic() -> NimBLECharacteristic* { return pJsonTxCharacteristic; }
     void begin();
     void end();
 };
